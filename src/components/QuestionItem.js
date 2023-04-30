24789f28a20f9d996b355b174b955da2dd7faa32
Erickkirui <erickkirui653@gmail.com>
2023-04-30 05:46:13 +0300

    impliment fetch

--- src/components/QuestionItem.js
+++ src/components/QuestionItem.js
@@ -2,12 +2,14 @@ import React from "react";
 
 function QuestionItem({ question }) {
   const { id, prompt, answers, correctIndex } = question;
+  
 
-  const options = answers.map((answer, index) => (
+  const options = answers?.map((answer, index) => (
     <option key={index} value={index}>
       {answer}
     </option>
   ));
+// do out fetch here
 
   return (
     <li>
